70966aa010c5e549f5d72119310924818216c9a2
Afroz-215 <afrozj215@gmail.com>
2025-09-02 19:59:29 +0530

    crud operation

---
+++ index.js
@@ -0,0 +1,31 @@
+// // NodeJS +ExpressJS +DB
+
+// // js =>NodeJS => file FIlesystem => server
+
+
+// // console.log("first")
+
+// console.log(globalThis)
+// console.log(document)
+
+// a()
+// var a=10;
+// var a=()=>{
+//     console.log(a);
+// }
+
+
+// const a
+// a = 5;
+// console.log(a)
+
+
+// var a="hello"
+// a[0]='z'
+// a="byee"
+// console.log(a)
+
+
+// console.log(null+1)
+
+git 
