da22ce21e684acb6956eb5174acfbe0a7021d81c
Afroz-215 <afrozj215@gmail.com>
2025-09-07 20:12:47 +0530

    routes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@
 // console.log(null+1)
 
 
-let fs = require("fs")
+// let fs = require("fs")
 
 
 // fs.writeFileSync("index.html","hello fs , we are learng backend")
@@ -81,11 +81,125 @@ let fs = require("fs")
 // // let text = fs.readFileSync("index.js", "utf8");
 // // console.log(text);
 
-fs.readFile("index.js", "utf8", (err, data) => {
-  if (err) {
-    console.error("Error:", err);
-    return;
-  }
-  console.log(data); // prints file content
-  console.log("Done reading (async)");
-});
+// fs.readFile("index.js", "utf8", (err, data) => {
+//   if (err) {
+//     console.error("Error:", err);
+//     return;
+//   }
+//   console.log(data); // prints file content
+//   console.log("Done reading (async)");
+// });
+
+
+//  Server Creating in node
+
+// let http =require("http")
+
+// let server =http.createServer((req,res)=>{
+//   res.send("hello from server")
+// })
+// server.listen(3000,()=>{
+//   console.log("server running on port no 3000")
+// })
+
+// console.log(http,"hehe")
+
+
+
+// Data of laptop
+
+// let os =require("os")
+// console.log(os.type())
+// console.log(os.platform())
+// console.log(os.arch())
+// console.log(os.platform())
+// console.log(os.freemem())
+// console.log(os.homedir())
+// console.log(os.hostname());
+
+
+// create server with express
+
+// let express =require("express")
+// let app =express()
+
+// app.use((req,res,next)=>{
+//   // res.send("these are middleware")
+//   next()
+// })
+
+// app.get('/',(req,res)=>{
+//   res.send("hello from express server")
+// })
+// app.get("/home",(req,res)=>{
+//   res.send("hello from homeeeeee ")
+// })
+// app.get('/about',(req,res)=>{
+//   res.send("hello from about section")
+// })
+// app.listen(4000,()=>{
+//   console.log("server running on 4000")
+// })
+
+
+
+
+
+
+
+// let fs =require('fs')
+// let express =require("express")
+// let app =express()
+
+// app.get("/",(req,res)=>{
+//   res.send("hello from server")
+// })
+
+// app.get('/create',(req,res)=>{
+//   fs.writeFileSync('index.html',"hello from writeFileSync")
+//   res.send("file created")
+// })
+
+// app.get('/read',(req,res)=>{
+//   let data =fs.readFileSync('index.html')
+//   res.send(data.toString())
+// })
+
+// app.get('/append',(req,res)=>{
+//   fs.appendFileSync('index.html',"append something")
+//   res.send('file appended')
+// })
+
+// app.get('/delete',(req,res)=>{
+//   fs.unlinkSync('index.html')
+//   res.send('delete file')
+// })
+
+// app.listen(4000,()=>{
+//   console.log('server running on port 4000')
+// })
+
+
+
+let express = require('express')
+let app = express()
+
+app.get('/:ani', (req, res) => {
+  let { ani } = req.params
+  res.send(ani)
+  // console.log(req);
+  // res.send(req)
+  // req.send("heee")
+})
+//   app.get("/cat",(req,res)=>{
+//     res.send("cat")
+//     // console.log(req);
+//     // res.send(req)
+//     // req.send("heee")
+
+//   })
+
+
+app.listen(4000, () => {
+  console.log("first")
+})
